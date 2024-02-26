497ff387aa4040ae3afe3593dbefaafe48882929
HendMohammed90 <hend.odesk.tasks@gmail.com>
2024-02-26 23:07:38 +0200

    add howItWork section

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/header/Header'
 import Home from '../components/Home/Home'
 import About from '../components/about/About'
+import Work from '../components/workSample/Work'
 
 const Main = () => {
   return (
@@ -9,6 +10,7 @@ const Main = () => {
       <Header />
       <Home />
       <About />
+      <Work />
     </div>
   )
 }
