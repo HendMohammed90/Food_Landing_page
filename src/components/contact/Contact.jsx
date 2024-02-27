353044e4d8e9818b978447b094f1559a7d900854
HendMohammed90 <hend.odesk.tasks@gmail.com>
2024-02-27 20:11:39 +0200

    update contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,7 +6,9 @@ const Contact = () => {
             <h1 className='primary-heading'>Have Question In Mind?
                 Let Us Help You</h1>
             <div className='contact-form-container'>
-                <input type='email' placeholder='yourmail@gmail.com'/>
+            <input type='text' placeholder='Name'/>
+                <input type='email' placeholder='Email' onFocus={(e)=> e.target.placeholder = ''}/>
+                <textarea type='text' placeholder='Write Your Message'/>
                 <button type='submit' className='secondary-button'>Submit</button>
             </div>
         </section>
